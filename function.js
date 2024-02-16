b28fa5c3e4d49712710c12dd89177d89fb71c3e1
Guffy-san <vinaybasargekar13@gmail.com>
2024-02-16 23:11:43 +0530

    added a simpler javascript concept

--- function.js
+++ function.js
@@ -1,22 +1,75 @@
-document.addEventListener("DOMContentLoaded", function () {
-	// Add event listener for keyboard key presses
-	window.addEventListener("keydown", function (event) {
-		const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-		const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-		if (!audio) return; // Stop the function if there's no audio element associated with the pressed key
-		audio.currentTime = 0; // Rewind the audio to the start to allow for rapid consecutive presses
-		audio.play();
-		key.classList.add("playing"); // Add class for visual effect
-	});
+// Calling function on click over keys
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+	var crash = document
+		.querySelectorAll("button")
+		[i].addEventListener("click", function () {
+			var buttonText = this.childNodes[0].textContent.toLowerCase(); 
 
-	// Remove visual effect when transition ends
-	function removeTransition(event) {
-		if (event.propertyName !== "transform") return;
-		this.classList.remove("playing");
-	}
+			makeSound(buttonText);
 
-	const keysWithTransition = document.querySelectorAll(".key");
-	keysWithTransition.forEach((key) =>
-		key.addEventListener("transitionend", removeTransition)
-	);
+			buttonAnimation(buttonText);
+		});
+}
+
+// Calling function on pressed by the keys on keyboard
+document.addEventListener("keydown", function (event) {
+
+	makeSound(event.key.toLowerCase());
+
+	buttonAnimation(event.key.toLowerCase());
 });
+
+// function to play sound.
+function makeSound(key) {
+	switch (key) {
+		case "a":
+			var audio = new Audio("./sounds/clap.wav");
+			audio.play();
+			break;
+		case "s":
+			var audio = new Audio("./sounds/hihat.wav");
+			audio.play();
+			break;
+		case "d":
+			var audio = new Audio("./sounds/kick.wav");
+			audio.play();
+			break;
+		case "f":
+			var audio = new Audio("./sounds/openhat.wav");
+			audio.play();
+			break;
+		case "g":
+			var audio = new Audio("./sounds/boom.wav");
+			audio.play();
+			break;
+		case "h":
+			var audio = new Audio("./sounds/ride.wav");
+			audio.play();
+			break;
+		case "j":
+			var audio = new Audio("./sounds/snare.wav");
+			audio.play();
+			break;
+		case "k":
+			var audio = new Audio("./sounds/tom.wav");
+			audio.play();
+			break;
+		case "l":
+			var audio = new Audio("./sounds/tink.wav");
+			audio.play();
+			break;
+		default:
+			break;
+	}
+}
+
+// function to show the animation and call playing in CSS.
+function buttonAnimation(currentKey) {
+	var activeButton = document.querySelector("." + currentKey);
+
+	activeButton.classList.add("playing");
+
+	setTimeout(function () {
+		activeButton.classList.remove("playing");
+	}, 100);
+}
